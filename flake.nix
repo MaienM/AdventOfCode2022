e1037349acfd49e0bb53cb9165c41a7b1d45a289
Michon van Dooren <me@maienm.com>
2023-11-28 22:27:25 +0100

    Update flakes

--- flake.nix
+++ flake.nix
@@ -1,34 +1,35 @@
 {
-  description = "Advent of Code 2022";
-
   inputs = {
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs, fenix }:
-    defaultPackage.x86_64-linux = fenix.packages.x86_64-linux.minimal.toolchain;
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({ pkgs, ... }: {
-          nixpkgs.overlays = [ fenix.overlays.default ];
-          environment.systemPackages = with pkgs; [
-            (fenix.complete.withComponents [
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, fenix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        devToolchain = fenix.packages.${system}.stable;
+        devToolchainUnstable = fenix.packages."${system}".latest;
+      in
+      {
+        defaultPackage = fenix.packages.x86_64-linux.minimal.toolchain;
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            (devToolchain.withComponents [
               "cargo"
               "clippy"
               "rust-src"
               "rustc"
+            ])
+            (devToolchainUnstable.withComponents [
               "rustfmt"
             ])
-            rust-analyzer-nightly
+            fenix.packages.${system}.rust-analyzer
+            pkgs.cargo-make
           ];
-        })
-      ];
-    };
-    devShells.default {
-    };
-  };
+        };
+      });
 }
